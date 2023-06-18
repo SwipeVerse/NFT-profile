5579b526e3ecfbd550a60fe54eff5dd0f81f86ee
lakshay.gaur <lakshaygaur09@gmail.com>
2023-06-18 16:54:10 +0530

    fairplay contract - first draft

--- contracts/Fairplay.sol
+++ contracts/Fairplay.sol
@@ -3,42 +3,56 @@ pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log ..
 // import "hardhat/console.sol";
-import "./library/BinarySearchTree.sol";
+// import "./library/BinarySearchTree.sol";
 
 contract Fairplay {
-    BinarySearchTree.Tree tree;
-    BinarySearchTree.Node[] node;
+    enum Gender {
+        Male, // 0
+        Female // 1
+    }
 
-    mapping(address => BinarySearchTree.Node) public users;
+    struct player {
+        address addr;
+        string name; // short name (up to 32 bytes)
+        uint dob; //  uint date = 1638352800; // 2012-12-01 10:00:00
+        Gender gender; // Male/Female; 0/1
+        uint last_location; // https://stackoverflow.com/questions/8285599/
+        string uri;
+    }
+
+    mapping(address => player) public users;
+    mapping(address => address[]) public match_pool_users;
+    mapping(address => bool) public is_added;
 
     function getLastLocation(address addr) public view returns (uint) {
         return users[addr].last_location;
     }
 
+    function getUser() public view returns (player memory) {
+        return users[msg.sender];
+    }
+
     function addUser(
-        bytes32 name,
+        string calldata name,
         uint dob,
-        BinarySearchTree.Gender gender,
+        Gender gender,
         uint last_location
     ) public {
-        BinarySearchTree.add(
-            tree,
-            msg.sender,
-            name,
-            dob,
-            gender,
-            last_location
-        );
-        users[msg.sender] = BinarySearchTree.Node({
+        require(is_added[msg.sender] != true);
+        users[msg.sender] = player({
             addr: msg.sender,
             name: name,
             dob: dob,
             gender: gender,
             last_location: last_location,
-            left: 0,
-            right: 0
+            uri: ''
         });
+        is_added[msg.sender] = true;
     }
 
-    function findUsers() public {}
+    function generateMatchPool(uint search_radius) public {}
+
+    function getMatchPool() public view returns (address[] memory) {
+        return match_pool_users[msg.sender];
+    }
 }
--- scripts/add-user.ts
+++ scripts/add-user.ts
@@ -9,10 +9,14 @@ const Noida = { lat: 28.535517, lng: 77.391029 }
 const Delhi = { lat: 28.704060, lng: 77.102493 }
 const NewYork = { lat: 40.712776, lng: -74.005974 }
 const Argentina = { lat: -47.067197, lng: -69.408051 }
+const Jaipur = { lat: 26.912434, lng: 75.787270 }
+
+const SEARCH_RADIUS = 55 // in KMs
 
 const compressCoordinates = (location: Location): number => {
-  const WEI = 100000000
-  return ((location.lat + 90) * 180 + location.lng) * WEI;
+  const WEI = 100000000 // 10^8
+  console.log(Math.floor(((location.lat + 90) * 180 + location.lng) * WEI))
+  return Math.floor(((location.lat + 90) * 180 + location.lng) * WEI);
 }
 
 const web3StringToBytes32 = (text: string) => {
@@ -25,22 +29,34 @@ const web3StringToBytes32 = (text: string) => {
 
 async function main() {
   // Library deployment
-  const lib = await ethers.getContractFactory("BinarySearchTree");
-  const libInstance = await lib.deploy();
-  await libInstance.deployed();
-  console.log("Library Address--->" + libInstance.address)
+  // const lib = await ethers.getContractFactory("BinarySearchTree");
+  // const libInstance = await lib.deploy();
+  // await libInstance.deployed();
+  // console.log("Library Address--->" + libInstance.address)
 
-  const Fairplay = await ethers.getContractFactory("Fairplay", { libraries: { BinarySearchTree: libInstance.address } });
+  const Fairplay = await ethers.getContractFactory("Fairplay") //, { libraries: { BinarySearchTree: libInstance.address } });
   const fairplay = await Fairplay.deploy();
 
   await fairplay.deployed();
-  const [Mike] = await ethers.getSigners();
-  await fairplay.connect(Mike).addUser(web3StringToBytes32('Mike'), new Date('01-03-1991').getTime(), 0, compressCoordinates(NewYork));
-  // await fairplay.getUser();
-
   console.log(
     `Fairplay deployed to ${fairplay.address}`
   );
+  const [Mike, Ram, Jai, Bhaan] = await ethers.getSigners();
+  // date format: mm/dd/yyyy
+  await fairplay.connect(Mike).addUser(web3StringToBytes32('Mike'), new Date('01/03/1991').getTime(), 0, compressCoordinates(NewYork));
+  
+  await fairplay.connect(Ram).addUser(web3StringToBytes32('Ram'), new Date('11/15/1989').getTime(), 0, compressCoordinates(Noida));
+  
+  await fairplay.connect(Jai).addUser(web3StringToBytes32('Jai'), new Date('09/21/1997').getTime(), 0, compressCoordinates(Delhi));
+  
+  await fairplay.connect(Bhaan).addUser(web3StringToBytes32('Bhaan'), new Date('05/30/1978').getTime(), 0, compressCoordinates(Jaipur));
+  const MikeResult = await fairplay.connect(Mike).getUser();
+  const RamResult = await fairplay.connect(Ram).getUser();
+  console.log('Mike == ', MikeResult);
+  console.log('Ram == ', RamResult);
+  await fairplay.connect(Ram).generateMatchPool(SEARCH_RADIUS);
+  const matchPool = await fairplay.connect(Ram).getMatchPool();
+  console.log('Matchpool', matchPool);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
